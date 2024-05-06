575fe58ca83f758ed4df06f6aca7b322abf27f82
sameer khan <samkhan@sameers-MacBook-Air.local>
2024-05-06 19:09:03 +0530

    latest commit

---
+++ only-edu-test/src/app/testpage/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestPage = () => {
+  return <div className="w-screen h-screen">TestPage</div>;
+};
+
+export default TestPage;
--- only-edu-test/src/modules/layout/templates/footer/index.jsx
+++ only-edu-test/src/modules/layout/templates/footer/index.jsx
@@ -2,28 +2,31 @@ import Link from "next/link";
 
 export default async function Footer() {
   return (
-    <footer className="">
-    <div>
-      <div className="bg-[#9DCED3]">
-      <ul>
-          <li>ABOUT</li>
-        </ul>
-        <ul>
-          <li>ABOUT</li>
-        </ul>
-        <ul>
-          <li>HELP</li>
-        </ul>
+    <footer className=" ">
+      <div>
+        <div className="bg-[#9DCED3]">
+          <ul>
+            <li>ABOUT</li>
+          </ul>
+          <ul>
+            <li>ABOUT</li>
+          </ul>
+          <ul>
+            <li>HELP</li>
+          </ul>
+        </div>
+        <div className=" bg-[#115E5D] text-[#f8fafc] flex justify-between">
+          <p>&#169; 2024 Only Education. All rights reserved</p>
+          <ol className="flex">
+            <li>
+              <a href="">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="">Terms and Conditions</a>
+            </li>
+          </ol>
+        </div>
       </div>
-      <div className=" bg-[#115E5D] text-[#f8fafc] flex justify-between">
-        <p>&#169; 2024 Only Education. All rights reserved</p>
-      <ol className="flex">
-        <li><a href="">Privacy Policy</a></li>
-        <li><a href="">Terms and Conditions</a></li>
-      </ol>
-      </div>
-    </div>
-    
     </footer>
   );
 }
