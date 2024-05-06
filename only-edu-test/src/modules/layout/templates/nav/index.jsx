f63c6ccef10a20c467388c50e12dc22aec829bc6
sameer khan <samkhan@sameers-MacBook-Air.local>
2024-05-06 15:08:18 +0530

    added category cards and navbar

--- only-edu-test/src/modules/layout/templates/nav/index.jsx
+++ only-edu-test/src/modules/layout/templates/nav/index.jsx
@@ -1,51 +1,72 @@
-import { BsPerson } from "react-icons/bs";
+"use client";
+import React, { useState } from "react";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
+import { cn } from "@/util/cn";
 import Link from "next/link";
-import Logo from "@/modules/logo/logo";
 
-export default async function Nav() {
+export const FloatingNav = ({ navItems, className }) => {
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(false);
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
+    if (typeof current === "number") {
+      let direction = current - scrollYProgress.getPrevious();
+
+      if (scrollYProgress.get() < 0) {
+        setVisible(false);
+      } else {
+        if (direction < 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      }
+    }
+  });
+
   return (
-    <>
-      <div className="fixed top-0 inset-x-0 z-50 group  border-[#9c9c9c68] uppercase">
-        <header className="relative h-20 sm:h-[6rem]  mx-auto ">
-          <nav className=" flex items-center justify-end w-full h-full px-4">
-            <div className=" h-full flex items-center">
-              <div className="h-full ">
-                <div className="absolute top-4 left-4">
-                  <Logo />
-                </div>
-              </div>
-            </div>
-            <div className="px-5">
-              <div className="flex gap-x-8 text-sm absolute top-1/2 left-1/2 -translate-x-1/2  border -translate-y-1/2 capitalize bg-white px-20 rounded-full font-semibold ">
-                <button href="/" className="txt-compact-xlarge-plus p-3">
-                  Test
-                </button>{" "}
-                <button href="/" className="txt-compact-xlarge-plus p-1">
-                  Test
-                </button>{" "}
-                <button href="/" className="txt-compact-xlarge-plus p-1">
-                  Test
-                </button>{" "}
-                <button href="/" className="txt-compact-xlarge-plus p-1">
-                  Test
-                </button>
-              </div>
-            </div>
-            <div className="flex gap-x-5  justify-center items-center text-sm font-semibold">
-              <button class="cursor-pointer flex justify-between text-center text-black">
-                <span>Login</span>
-              </button>
-
-              <button class="hero-sticky-button whitespace-nowrap px-4 py-3 rounded-[21px] text-white  bg-[#fd7c22] justify-center relative overflow-hidden flex items-center font-normal duration-200 ease-in-out transition-colors">
-                <span className="flex flex-col overflow-hidden h-[1.2rem]">
-                  <span>Register</span>
-                  <span>Register</span>
-                </span>
-              </button>
-            </div>
-          </nav>
-        </header>
-      </div>
-    </>
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{
+          opacity: 1,
+          y: -100,
+        }}
+        animate={{
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        className={cn(
+          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
+          className
+        )}
+      >
+        {navItems.map((navItem, idx) => (
+          <Link
+            key={`link-${idx}`}
+            href={navItem.link}
+            className={cn(
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+            )}
+          >
+            <span className="block sm:hidden">{navItem.icon}</span>
+            <span className="hidden sm:block text-sm">{navItem.name}</span>
+          </Link>
+        ))}
+        <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+          <span>Login</span>
+          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+        </button>
+      </motion.div>
+    </AnimatePresence>
   );
-}
+};
--- only-edu-test/src/modules/test-cards/index.jsx
+++ only-edu-test/src/modules/test-cards/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const TestCards = () => {
   return (
-    <div className="aspect-auto bg-[#f2f0f0]  rounded-2xl border shadow-sm p-5">
-      <div className="flex justify-between">
+    <div className="aspect-auto bg-[#f2f0f0] rounded-2xl border shadow-sm p-5 grid  h-fit">
+      <div className="flex justify-between aspect-auto">
         <div className="flex">
           <div className="p-4 border rounded-md">
             <svg
@@ -31,7 +31,7 @@ const TestCards = () => {
             </svg>
           </div>
           <div className="py-2 ml-2 font-medium flex flex-col">
-            <h5>Core Java</h5>
+            <h5>Engneering</h5>
             <h5 className="text-xs text-slate-500">
               <span>Software and programming</span>
             </h5>
@@ -43,14 +43,25 @@ const TestCards = () => {
             </h5>
           </div>
         </div>
-        <div className="flex justify-around flex-col mt-4 text-sm">
-          <button className="bg-[#fd7c22] text-white font-medium border py-2 px-2 rounded-lg">
-            Start test
+        <div className="flex justify-around flex-col mt-4 text-xs">
+          <button className="bg-[#fd7c22] text-white font-medium border border-gray-400 py-2 px-2 rounded-full">
+            <span className="w-full">Start test</span>
           </button>
-          <button className="bg-[#d5d5d5] text-black font-medium border py-2 px-2 rounded-lg">
-            Save the test
+          <button className="bg-[#d5d5d5] text-black font-medium border border-gray-400 py-2 px-2 rounded-full">
+            <span className="w-full">Save the test</span>
           </button>
         </div>
+      </div>
+      <div className="options pt-4 flex flex-wrap aspect-auto gap-y-1 gap-x-1">
+        <button className="px-3 py-1 max-w-fit h-8 text-black text-xs rounded-full font-semibold hover:shadow-sm cursor-pointer border bg-gray-200 hover:bg-gray-200/70  ">
+          Mechanical engnieering
+        </button>
+        <button className="px-3 py-1  max-w-fit h-8 text-black text-xs rounded-full font-semibold hover:shadow-sm cursor-pointer border bg-gray-200  hover:bg-gray-200/70 ">
+          Electronic
+        </button>
+        <button className="px-3 py-1 max-w-fit h-8 text-black text-xs rounded-full font-semibold hover:shadow-sm cursor-pointer border bg-gray-200 hover:bg-gray-200/70 ">
+          Auto-mobile engineering
+        </button>
       </div>
     </div>
   );
