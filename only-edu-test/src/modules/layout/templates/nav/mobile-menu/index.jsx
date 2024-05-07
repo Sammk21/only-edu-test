39afa05813382a7c5aef56407a7f0a06ed87a19b
sameer khan <samkhan@sameers-MacBook-Air.local>
2024-05-07 17:30:15 +0530

    made navbar responsive and created a hamburger menu for mobile containg content of navbar

---
+++ only-edu-test/src/modules/layout/templates/nav/mobile-menu/index.jsx
@@ -0,0 +1,49 @@
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useEffect } from "react";
+
+const MobileMenu = ({ isOpen, toggleMenu }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    // Clean up function to re-enable scrolling when component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <>
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            className="fixed top-0 right-0 h-full w-2/3 bg-[#115e5d] shadow z-10"
+          >
+            <ul className="mt-20 text-xl">
+              <li className="py-2 px-4">Menu Item 1</li>
+              <li className="py-2 px-4">Menu Item 2</li>
+              <li className="py-2 px-4">Menu Item 3</li>
+            </ul>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 0.5 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="fixed top-0 left-0 h-full w-full bg-black "
+            onClick={toggleMenu} // Close menu when clicking outside
+          />
+        </>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default MobileMenu;
