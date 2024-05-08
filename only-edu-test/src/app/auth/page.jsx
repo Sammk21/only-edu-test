0fecaf1927f0a89b51341ce61c2d53a428dd2e46
onlyeducationpvt <sarfrajonlyeducation@gmail.com>
2024-05-08 20:04:43 +0530

    added login adn register

---
+++ only-edu-test/src/app/auth/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="text-center py-28 bg-gradient-to-r from-orange-100 to-teal-100 overflow-hidden">
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default page;
